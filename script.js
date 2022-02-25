c0e785acc3a022d1644512a14a2875d04ff87d2e
Redcoder807 <gearonhaynes@gmail.com>
2022-02-25 14:50:15 -0600

    done

---
+++ script.js
@@ -0,0 +1,50 @@
+const mcuShows = [`Loki`, `Moon Knight`];
+
+const starWarsShows = [`The Mandalorian`, `The Book of Boba Fett`];
+
+const disneyPlusShows = [...mcuShows, ...starWarsShows, `The Beatles: Get Back`];
+
+console.log(disneyPlusShows);
+
+const netflixMovies = {
+    action: `Extraction`,
+    crime: `The Irishman`
+};
+
+const amazonPrimeMovies = {
+    action: `The Tomorrow War`,
+    drama: `One Night In Miami`
+};
+
+const streamingMovies = {
+    ...amazonPrimeMovies,
+    ...netflixMovies,
+    musical: `Hamilton`
+};
+
+console.log(streamingMovies);
+
+const disneyJunior = [`Mickey Mouse Clubhouse`, `Spidey and His Amazing Friends`];
+
+const [mickey, spidey] = disneyJunior;
+
+console.log(mickey, spidey);
+
+const avengers = {
+    warMachine: `James Rhodes`,
+    theHulk: `Bruce Banner`
+}
+
+const {warMachine, theHulk} = avengers;
+
+console.log(warMachine, theHulk);
+
+const moreAvengers = {
+    blackWidow: `Natasha Romanoff`,
+    hawkeye: `Clint Barton`,
+    ironMan: `Tony Stark`
+}
+
+const {blackWidow: nat, ...other} = moreAvengers;
+
+console.log(nat, other);
